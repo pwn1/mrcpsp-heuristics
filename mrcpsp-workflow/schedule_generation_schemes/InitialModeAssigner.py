3683dcb42eb776fedc29a6bd3457528a01108757
George Jopson <georgejopson@gmail.com>
2026-06-25 18:27:50 +0100

    Improve typing in InitialModeAssigner.py

--- mrcpsp-workflow/schedule_generation_schemes/InitialModeAssigner.py
+++ mrcpsp-workflow/schedule_generation_schemes/InitialModeAssigner.py
@@ -1,22 +1,43 @@
-from typing import Protocol
+from typing import Protocol, Callable
+
+from mrcpsp import Project
+from schedule_generation_schemes.schedulers import Scheduler
+
 
 class InitialModeAssigner(Protocol):
     @staticmethod
-    def assign_modes(project, priority_fn, mode_fn, core): ...
+    def assign_modes(
+            project: Project,
+            priority_fn: Callable,
+            mode_fn: Callable,
+            core_scheduler: Scheduler
+    ) -> list[int]: ...
+
 
 class ContextAwareModeAssigner:
     @staticmethod
-    def assign_modes(project, priority_fn, mode_fn, core):
+    def assign_modes(
+            project: Project,
+            priority_fn: Callable,
+            mode_fn: Callable,
+            core_scheduler: Scheduler
+    ) -> list[int]:
         proxy_modes = [min(range(len(a.modes)), key=lambda m: a.modes[m].duration)
                        for a in project.activities]
         priorities = priority_fn(project=project, mode_assignments=proxy_modes)
         mode_assignments = [0] * project.num_activities
-        core.context_aware_pass(project, priorities, mode_assignments, mode_fn=mode_fn)
+        core_scheduler.context_aware_pass(project, priorities, mode_assignments, mode_fn=mode_fn)
         return mode_assignments
+
 
 class ContextUnAwareModeAssigner:
     @staticmethod
-    def assign_modes(project, priority_fn, mode_fn, core):
+    def assign_modes(
+            project: Project,
+            priority_fn: Callable,
+            mode_fn: Callable,
+            core_scheduler: Scheduler
+    ) -> list[int]:
         return [
             mode_fn(activity=project.activities[i]) for i in range(project.num_activities)
         ]
