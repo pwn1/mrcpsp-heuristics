3b4600ff4a26b906f75f1cf7d77f6d3b8ef0f961
George Jopson <georgejopson@gmail.com>
2026-07-01 15:37:04 +0100

    Add comment describing novel approach taken in ScheduleGenerator.py

--- mrcpsp-workflow/schedule_generation_schemes/ScheduleGenerator.py
+++ mrcpsp-workflow/schedule_generation_schemes/ScheduleGenerator.py
@@ -20,6 +20,24 @@ class ScheduleGenerator:
         self.initial_mode_assigner = initial_mode_assigner
 
     def run(self, project: Project) -> Schedule | None:
+        """
+        Returns schedule for the project.
+
+        This is inspired from the general algorithm described by A. Lova, P. Tormos,
+        and F. Barber. However, they describe a single pass approach, where a context
+        aware mode rule (EFFT) is used to assign the modes with a single S-SGS/P-SGS
+        pass.
+
+        However, unlike A. Lova, P. Tormos, and F. Barber, we consider non-renewable
+        resources as well. This means that we take a two pass approach. This first pass
+        for context aware mode heuristics is the same as described. However, once
+        this has been completed we take these modes and use a local search algorithm
+        to try and repair any non-renewable resource violations. Once we have done this,
+        we do another pass of the S-SGS/P-SGS, but set the modes to those found by this
+        initial process. (Note: For context unaware mode heuristics, only a single pass
+        is completed, as we set the modes statically at the beginning.)
+        """
+
         mode_assignments = (
             self
             .initial_mode_assigner
